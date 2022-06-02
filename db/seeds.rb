ee841312d96155e76a62e557690d1084ae07d1bf
Mergen Battur <mbattur@yahoo.com>
2022-06-01 20:53:17 -0500

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,41 @@
+airlines = Airline.create([
+  { 
+    name: "United Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+  }, 
+  { 
+    name: "Southwest",
+    image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png"
+  },
+  { 
+    name: "Delta",
+    image_url: "https://open-flights.s3.amazonaws.com/Delta.png" 
+  }, 
+  { 
+    name: "Alaska Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png" 
+  }, 
+  { 
+    name: "JetBlue",
+    image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png" 
+  }, 
+  { 
+    name: "American Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png" 
+  }
+])
+
+reviews = Review.create([
+  {
+    title: "Greate airline",
+    description: "I had a lovely time",
+    score: 5,
+    airline: airlines.first
+  },
+  {
+    title: "Bad airline",
+    description: "I didn't had a lovely time",
+    score: 2,
+    airline: airlines.first
+  },
+])
